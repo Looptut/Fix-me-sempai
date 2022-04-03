942aa49e71a9f4be10151e09e710649d7a1be537
Alina Svintsova <aline.svintsova@gmail.com>
2022-04-04 01:00:21 +0700

    tutorial and changes

--- Assets/Scripts/Game/FireControl.cs
+++ Assets/Scripts/Game/FireControl.cs
@@ -1,17 +1,149 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-[RequireComponent(typeof(ProgressBar))]
+
+[Serializable]
+public struct Level
+{
+    [Header("Пройдено время с начала игры для этого уровня")]
+    public int LevelTime;
+    [Header("Интервал для спавна работников")]
+    public int Interval;
+    [Header("Работников возгарает каждый интервал")]
+    public int WorkersCount;
+}
+
 public class FireControl : MonoBehaviour
 {
+    [SerializeField]
+    [Header("Время появление каждого босса")]
+    private List<int> bossTimer = new List<int>() { 10, 20, 30 };
+    [SerializeField]
+    [Header("Уровни")]
+    private List<Level> levels;
+
+    private List<Worker> workers;
+    private List<Worker> bosses;
+
+    private float startTime;
+    private float lastWorkerFire = 0f;
+
+    private int currLevelIndex = 0;
+
+    public bool isTestingWithoutTutorial = false;
+
+    private void Start()
+    {
+        TutorialDialog.OnTutorialEnds += StartControl;
+        if (isTestingWithoutTutorial)
+        {
+            StartControl();
+        }
+    }
+
+    private void FindWorkers()
+    {
+        workers = new List<Worker>(FindObjectsOfType<Worker>().Where((c) => !c.IsBeingBoss));
+        bosses = new List<Worker>(FindObjectsOfType<Worker>().Where((c) => c.IsBeingBoss));
+    }
+
+    public void StartControl()
+    {
+        FindWorkers();
+        startTime = Time.time;
+        StartCoroutine(Control());
+    }
+
+    private IEnumerator Control()
+    {
+        while (enabled)
+        {
+            CheckLevel();
+            SpawnWorkerFire();
+            SpawnBossFire();
+
+            yield return null;
+        }
+    }
+
+    private void CheckLevel()
+    {
+        for (int i = currLevelIndex; i < levels.Count; i++)
+        {
+            if (Time.time - startTime >= levels[i].LevelTime)
+            {
+                currLevelIndex = i;
+            }
+        }
+    }
+
+    private void SpawnWorkerFire()
+    {
+        if (lastWorkerFire + levels[currLevelIndex].Interval > Time.time - startTime)
+            return;
+
+        int spawnCount = Math.Min(levels[currLevelIndex].WorkersCount, workers.Count);
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            int rand = Random.Range(0, workers.Count);
+            Worker worker = workers[rand];
+            worker.IsBeingTired = true;
+
+            workers.Remove(worker);
+            worker.OnAction += ReturnWorkerToPool;
+        }
+        lastWorkerFire = Time.time;
+    }
+
+    private void SpawnBossFire()
+    {
+        for (int i = 0; i < bossTimer.Count; i++)
+        {
+            if (Time.time - startTime > bossTimer[i])
+            {
+                if (bosses.Count == 0)
+                {
+                    Debug.LogError("Боссы закончились!");
+                    return;
+                }
+
+                int rand = Random.Range(0, bosses.Count);
+                Worker boss = bosses[rand];
+                boss.IsBeingTired = true;
+                bosses.Remove(boss);
+                bossTimer.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    private void ReturnWorkerToPool(Worker worker)
+    {
+        worker.OnAction -= ReturnWorkerToPool;
+        workers.Add(worker);
+    }
+    private void OnDestroy()
+    {
+        TutorialDialog.OnTutorialEnds -= StartControl;
+    }
+
+    /*
     private const string WORKER_TAG = "Worker";
     private List<Worker> workers = new List<Worker>();
     private List<Worker> workersSelected = new List<Worker>();
     private Worker worker;
+    private FireCount maxFireCount;
+    private float timeCount = 0;
+    private float spawnTime = 0;
 
     public FireCount fireCount;
     public ProgressBar progressBar;
+    public int spawnInterval;
 
     public class FireCount 
     {
@@ -25,7 +157,8 @@ public class FireControl : MonoBehaviour
 
     private void Awake()
     {
-        fireCount = new FireCount();
+        fireCount = new FireCount() { level = 1, workers = 0, bosses = 0, time = 0 }; 
+        maxFireCount = new FireCount();
         var array = GameObject.FindGameObjectsWithTag(WORKER_TAG);
         for (int i = 0; i < array.Length; i++)
         {
@@ -38,25 +171,29 @@ public class FireControl : MonoBehaviour
 
     private void Start()
     {
-        InvokeRepeating("setFire", 0, 1.0f);
+        InvokeRepeating(nameof(setFire), 0, 1.0f);
     }
     void Update()
     {
-
+        timeCount += Time.deltaTime;
     }
 
     private void setFire() 
     {
         int points = progressBar.GetCurrentPoints();
-        if (points < 30) fireCount.level = 1;
-        if (points > 29 && points < 70) fireCount.level = 2;
-        if (points > 69) fireCount.level = 3;
+        if (points < 30) maxFireCount.level = 1;
+        if (points > 29 && points < 70) maxFireCount.level = 2;
+        if (points > 69) maxFireCount.level = 3;
 
-        UpdateFireCount();
+        UpdateMaxFireCount();
+
+        if (timeCount - spawnTime > spawnInterval)        
+            SpawnFire();
+        
 
         workersSelected = new List<Worker>();
         foreach (Worker ow in workers)
-            if (!ow.IsBeingTired && !ow.IsBeingBoss())
+            if (!ow.IsBeingTired && !ow.IsBeingBoss)
                 workersSelected.Add(ow);
         while (workers.Count - workersSelected.Count < fireCount.workers)
         {
@@ -77,25 +214,49 @@ public class FireControl : MonoBehaviour
         //}
     }
 
-    private void UpdateFireCount() 
+    private void SpawnFire() 
     {
-        switch (fireCount.level) 
+        spawnTime += spawnInterval;
+
+        workersSelected = new List<Worker>();
+        if (fireCount.workers < maxFireCount.workers)
+        {
+            foreach (Worker ow in workers)
+                if (!ow.IsBeingTired && !ow.IsBeingBoss)
+                    workersSelected.Add(ow);
+            workersSelected[Random.Range(0, workersSelected.Count)].IsBeingTired = true;
+            fireCount.workers++;
+        }
+        else if (fireCount.bosses < maxFireCount.bosses)
+        {
+            foreach (Worker ow in workers)
+                if (!ow.IsBeingTired && ow.IsBeingBoss)
+                    workersSelected.Add(ow);
+            workersSelected[Random.Range(0, workersSelected.Count)].IsBeingTired = true;
+            fireCount.bosses++;
+        }
+    }
+
+    private void UpdateMaxFireCount() 
+    {
+        switch (maxFireCount.level) 
         {
             case 1:
-                fireCount.workers = 3;
-                fireCount.bosses = 0;
-                fireCount.time = 20;
+                maxFireCount.workers = 3;
+                maxFireCount.bosses = 0;
+                maxFireCount.time = 20;
                 break;
             case 2:
-                fireCount.workers = 5;
-                fireCount.bosses = 1;
-                fireCount.time = 20;
+                maxFireCount.workers = 5;
+                maxFireCount.bosses = 1;
+                maxFireCount.time = 20;
                 break;
             case 3:
-                fireCount.workers = 5;
-                fireCount.bosses = 2;
-                fireCount.time = 15;
+                maxFireCount.workers = 5;
+                maxFireCount.bosses = 2;
+                maxFireCount.time = 15;
                 break;
         }
     }
+    */
 }
--- Assets/Scripts/Worker/Worker.cs
+++ Assets/Scripts/Worker/Worker.cs
@@ -6,17 +6,24 @@ using UnityEngine.Events;
 
 public class Worker : MonoBehaviour
 {
+    public static event Action ATTENTION_BOSS_ON_FIRE = delegate { };
+
     public UnityEvent ActionDone = new UnityEvent();
 
     public event Action<Worker> OnAction = delegate { };
     public event Action<Worker> OnBeingTired = delegate { };
     public static Action OnBeingBoss = delegate { };
 
-    [SerializeField] private BossFightController boss;
+    public Transform BubblePosition;
+    public Transform CheckPoint => checkPoint;
+
+    [SerializeField] private BossFightController bossFightController;
     [SerializeField] private PlayerInput player;
     [SerializeField] private Transform fire;
     [SerializeField] private bool isBeingTired = false;
     [SerializeField] private bool isBeingBoss = false;
+    [SerializeField] private Transform checkPoint;
+
     public bool IsBeingTired
     {
         get => isBeingTired;
@@ -27,6 +34,8 @@ public class Worker : MonoBehaviour
                 isBeingTired = value;
                 if (isBeingTired)
                 {
+                    if (IsBeingBoss)
+                        ATTENTION_BOSS_ON_FIRE();
                     OnBeingTired.Invoke(this);
                     if (fire != null)
                         fire.gameObject.SetActive(true);
@@ -34,14 +43,12 @@ public class Worker : MonoBehaviour
             }
         }
     }
-    public bool IsBeingBoss
-    {
-        get => isBeingBoss;
-        set => isBeingBoss = value;
-    }
+    public bool IsBeingBoss => isBeingBoss;
 
     private void Start()
     {
+        bossFightController = FindObjectOfType<BossFightController>();
+
         if (fire != null)
             fire.gameObject.SetActive(false);
     }
@@ -52,25 +59,33 @@ public class Worker : MonoBehaviour
     {
         IsBeingTired = !IsBeingTired;
     }
-    [ContextMenu("Switch boss")]
-    public void SwitchBoss()
-    {
-        IsBeingBoss = !IsBeingBoss;
-    }
 
     public virtual void PerformAction()
     {
         OnAction.Invoke(this);
 
-        if (IsBeingTired)
+        if (IsBeingTired && !IsBeingBoss)
         {
             fire.gameObject.SetActive(false);
+            IsBeingTired = false;
             player.DoAction(fire);
         }
-        if (isBeingBoss)
+        if (IsBeingTired && IsBeingBoss)
         {
-            boss.StartBossFight();
+            BossFightController.onEndFight += OnEndFight;
+            bossFightController.StartBossFight();
         }
         ActionDone.Invoke();
+    }
+
+    private void OnEndFight(bool isSuccess)
+    {
+        if (isSuccess)
+        {
+            fire.gameObject.SetActive(false);
+            IsBeingTired = false;
+            player.DoAction(fire);
+        }
+        BossFightController.onEndFight -= OnEndFight;
     }
 }
