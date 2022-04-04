22fdec87da2e7ddf0d82bbdccc6fdcf4a006bbf7
Alina Svintsova <aline.svintsova@gmail.com>
2022-04-05 01:54:15 +0700

    fix ambi

--- Assets/Scripts/AmbiController.cs
+++ Assets/Scripts/AmbiController.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AmbiController : MonoBehaviour
 {
-    [SerializeField] private AudioSource source;
+    private AudioSource source;
     void Start()
     {
+        source = GetComponent<AudioSource>();
+
         BossFightController.onStartFight += HandleAudio;
         BossFightController.onEndFight += HandleUnMute;
     }
